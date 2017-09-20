714a95b91a3e54a97ff98dd500a3c5b93441cf1c
Zero <silverfantacy@gmail.com>
2017-09-20 17:21:06 +0800

    部分修改,已知map bug

--- source/js/all.js
+++ source/js/all.js
@@ -20,26 +20,63 @@ function initMap() {
 }
 
 $(document).ready(function(){
+    $(".banner").addClass("animated fadeInLeftBig");
     // nav flex 
     $(window).on("scroll",function(){
         var scrolDistance = $(window).scrollTop(); //滾動距離
         var $nav = $("nav");
         if(scrolDistance > 155) {
             $nav.addClass("fixed-top");
+            // $("nav").css("background-color","rgba(29, 63, 123, 1");
         }
         else{
             $nav.removeClass("fixed-top");
+            // $("nav").css("background-color","rgba(29, 63, 123, 0.85");
         };
-        if(scrolDistance > 400) {
-            $(".about-content").css("display","flex");
-            $(".about-content").addClass("fadeInLeftBig");
-        };
-        
+        // if(scrolDistance > 400) {
+        //     $(".about-content").css("display","flex");
+        //     $(".about-content").addClass("fadeInLeftBig");
+        //     
+        // };
     })
+    // 滾動選單
+    // $("introduction-btn").on('click', function (e) {
+    //     console.log('123');
+    // });
+    // $('.introduction-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#introduction').offset().top
+    //     }, 1000);
+    // });
+    // $('.location-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#location').offset().top
+    //     }, 1000);
+    // });
+    // $('.traffic-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#traffic').offset().top
+    //     }, 1000);
+    // });
+    // $('.information-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#information').offset().top
+    //     }, 1000);
+    // });
+    // $('.meeting-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#meeting').offset().top
+    //     }, 1000);
+    // });
+    // $('.credit-btn').click(function () {
+    //     $('html,body').animate({
+    //         scrollTop: $('#credit').offset().top
+    //     }, 1000);
+    // });
     // traffic ul 
     $(".collapsed").on("click",function(){
         // console.log(123);
         $(this).parent().parent().parent().siblings().find(".card-header").removeClass("bottom-border-none");
         $(this).parent().parent().toggleClass("bottom-border-none");
-    })
+    });
 });
